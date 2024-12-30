10bfab5a981d2067a74ff0f02e1b947b40e7311e
German Walton <waltongerman@gmail.com>
2024-12-30 15:53:06 -0300

    Delete dist directory

--- dist/utils/error.util.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.formatValidationErrors = formatValidationErrors;
-function formatValidationErrors(errors) {
-    return errors.array().map(error => error.msg).join(", ");
-}
